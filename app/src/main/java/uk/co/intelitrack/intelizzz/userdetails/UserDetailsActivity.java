49951398fb1e559f7299416e03077b53a36167db
markodonovski <markodonovski@live.com>
2018-06-04 20:32:07 +0200

    Dodaden ekran 12

---
+++ app/src/main/java/uk/co/intelitrack/intelizzz/userdetails/UserDetailsActivity.java
@@ -0,0 +1,15 @@
+package uk.co.intelitrack.intelizzz.userdetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.intelitrack.intelizzz.R;
+
+public class UserDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+    }
+}
